f8a8c3160476f67e6702c9c9a048f6dcdea0856a
akshay-nair <akshay.nair@mail.utoronto.ca>
2016-09-26 13:20:30 -0400

    Materialize design, added datepicker, fixed asset links, added more form sections, cleaned up CSS, added accordions, fixed local storage

--- app/app.js
+++ app/app.js
@@ -1,48 +1,46 @@
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    onClose: function(val) {
+        val = this.get();
+
+        var modelName = this.$node.attr("ng-model");
+        var scope = angular.element(this.$node).scope();
+        scope.$apply(function() {
+            scope.data[modelName] = val;
+        });
+    }
+  });
+
   angular.module('SponsorForm', ['LocalStorageModule'])
     .controller('FormController', ['$scope','localStorageService', function($scope, localStorageService) {
 
 
     	new Imgur({
             clientid: 'cc86a8de0e7c459',
-            callback: feedback
+            callback: function (res) {
+	            if (res.success === true) {
+
+	              $scope.data.hackathon_logo_url = res.data.link;
+	               console.log(res.data.link);
+	            }
+	        }
         });
 
-  // To add to local storage
-  localStorageService.set('localStorageKey','Test123');
-
-  // Read that value back
-  var value = localStorageService.get('localStorageKey');
-
-
-
-  //$scope.thisCanBeusedInsideNgBindHtml = $sce.trustAsHtml('<label>test123</label>');
- //$scope.thisCanBeusedInsideNgBindHtml = '<label>test123</label>';
-  
-
-  //For the MM/HH field you need a 'input mask' 
-
-  	  $scope.master = {};
-
   	  $scope.data = {
-  	  	hackathon_name: "",
         hackathon_logo_url: "",
         hackathon_type: {
           University: false,
           Company: false,
           HighSchool: false,
-          Other: false },
-        hackathon_start_date: new Date(2016, 9, 22),
-        hackathon_end_date:  new Date(2016, 9, 23),
+          Other: false
+      	},
         hackathon_timetable: [
           {date: "", from: "", to: "", event_desc: "Registration"},
           {date: "", from: "", to: "", event_desc: "Hacking Starts"},
           {date: "", from: "", to: "", event_desc: "Sponsor Presentation"},
           {date: "", from: "", to: "", event_desc: "Closing Ceremony"}
         ],
-        hackathon_location: "",
         hackathon_floorplan_urls: {},
-        hackathon_size: 0,
-        hackathon_desc: "",
         hackathon_travel: false,
         hackathon_links: {
         	h_website: "",
@@ -60,9 +58,9 @@
         hackathon_contact: ""
   	  };
 
-
-      $scope.update = function(current) {
-        $scope.master = angular.copy(current);
+      $scope.update = function() {
+        $scope.master = angular.copy($scope.data);
+  		localStorageService.set('localStorageKey', JSON.stringify($scope.master));
       };
 
       $scope.reset = function() {
@@ -71,7 +69,7 @@
 	  
 	  $scope.removeRow = function(time) {
 		  var index = $scope.data.hackathon_timetable.indexOf(time);
-		  if (index >= 0) {
+		  if (index >= 0 && $scope.data.hackathon_timetable.length > 1) {
 			$scope.data.hackathon_timetable.splice(index, 1);
 		  }
 	  };
@@ -80,17 +78,13 @@
 		  $scope.data.hackathon_timetable.push({date: "", from: "", to: "", event_desc: ""});
 	  };
 
-      //$scope.reset();
-
-
-       var feedback = function (res) {
-            if (res.success === true) {
-
-              $scope.data.hackathon_logo_url = res.data.link;
-               console.log(res.data.link);
-            }
-        };
-
+    	try {
+  	  		$scope.master = JSON.parse(localStorageService.get('localStorageKey'));
+  	  		$scope.reset();
+  	  	} catch(e) {
+  	  		$scope.master = {};
+  	  		localStorageService.set('localStorageKey', JSON.stringify($scope.master));
+  	  	}
     
 
 
